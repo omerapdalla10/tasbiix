e8e3019152d18369bb6d11c30bff0279b222a5f9
omar <omerapdalla10@gmail.com>
2025-02-12 12:43:48 +0200

    tasbiix

---
+++ main.js
@@ -0,0 +1,16 @@
+document.querySelector('.button').addEventListener('click', function(){
+    a = document.querySelector('.ekran')
+    a.value = parseInt(a.value) +1
+    console.log(a.value);
+
+})
+
+
+
+document.querySelector('.delete').addEventListener('click', function(){
+    a = document.querySelector('.ekran')
+    ochirish = 0
+    a.value = ochirish
+    console.log(ochirish);
+})
+
