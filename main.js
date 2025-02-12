5483ed0b7e43e52408426e5e16444735238327a3
omar <omerapdalla10@gmail.com>
2025-02-12 12:56:13 +0200

    last update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector('.button').addEventListener('click', function(){
-    a = document.querySelector('.ekran')
+    a = document.querySelector('.om')
     a.value = parseInt(a.value) +1
     console.log(a.value);
 
@@ -7,8 +7,8 @@ document.querySelector('.button').addEventListener('click', function(){
 
 
 
-document.querySelector('.delete').addEventListener('click', function(){
-    a = document.querySelector('.ekran')
+document.querySelector('.remove').addEventListener('click', function(){
+    a = document.querySelector('.om')
     ochirish = 0
     a.value = ochirish
     console.log(ochirish);
